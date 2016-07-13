c01c1254d0c46ede9ce2c95c7869a46ab5e1ccbd
Midor1 <Midor1@outlook.com>
2016-07-13 11:39:09 +0800

    Added the internal document.

--- app/src/main/java/pw/midor/matrixcalculator/InitActivity.java
+++ app/src/main/java/pw/midor/matrixcalculator/InitActivity.java
@@ -3,6 +3,7 @@ package pw.midor.matrixcalculator;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -19,6 +20,11 @@ import com.alibaba.sdk.android.push.CloudPushService;
 import com.alibaba.sdk.android.push.CommonCallback;
 import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +51,8 @@ public class InitActivity extends Activity{
         lv = (ListView)findViewById(R.id.listView);
         list.add("矩阵计算器");
         list.add("矩阵运算练习");
+        list.add("正定性与二次型");
+        list.add("公式与定理整理");
         list.add("关于");
         list.add("退出");
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
@@ -60,6 +68,24 @@ public class InitActivity extends Activity{
                     Intent intent = new Intent(This,DifficultySelectionActivity.class);
                     startActivity(intent);
                 }
+                if(list.get(position).equals("正定性与二次型")) {
+                    Intent intent = new Intent(This,DifficultySelectionActivity.class);
+                    startActivity(intent);
+                }
+                if(list.get(position).equals("公式与定理整理")) {
+                    Intent intent = new Intent("android.intent.action.VIEW");
+                    intent.addCategory("android.intent.category.DEFAULT");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    try {
+                        copyBigDataToSD(android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/LinearAlgebra.doc");
+                    }
+                    catch(Exception e) {
+
+                    }
+                    Uri uri = Uri.fromFile(new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath()+"/LinearAlgebra.doc"));
+                    intent.setDataAndType(uri, "application/msword");
+                    startActivity(intent);
+                }
                 if(list.get(position).equals("关于")) {
                     Intent intent = new Intent(This,AboutActivity.class);
                     startActivity(intent);
@@ -69,6 +95,24 @@ public class InitActivity extends Activity{
                 }
             }
         });
+    }
+
+    private void copyBigDataToSD(String strOutFileName) throws IOException
+    {
+        InputStream myInput;
+        OutputStream myOutput = new FileOutputStream(strOutFileName);
+        myInput = this.getAssets().open("LinearAlgebra.doc");
+        byte[] buffer = new byte[1024];
+        int length = myInput.read(buffer);
+        while(length > 0)
+        {
+            myOutput.write(buffer, 0, length);
+            length = myInput.read(buffer);
+        }
+
+        myOutput.flush();
+        myInput.close();
+        myOutput.close();
     }
 
     private void initCloudChannel(Context applicationContext) {
